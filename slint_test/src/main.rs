53251b9f8d8698919f136a439bab5ea53e89751e
RezoTheProger <rezorima931@gmail.com>
2024-10-23 22:39:36 +0100

    client side update(not working yet)

--- slint_test/src/main.rs
+++ slint_test/src/main.rs
@@ -2,56 +2,54 @@
 
 use std::{error::Error, rc::Rc};
 use slint::{ModelRc, SharedString, VecModel};
-use tokio::{net::TcpStream, sync::mpsc};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream};
 use tokio_tungstenite::accept_async;
 slint::include_modules!();
-use futures::SinkExt;
 
+
+const ADDR:&str = "localhost:6666";
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     
     let ui = AppWindow::new()?;
-    let (tx, mut rx) = mpsc::unbounded_channel();
     let ui_handle = ui.as_weak();
-    
-    // Sending messages from the UI to Tokio task
-    ui.on_send_message({
-        let tx_clone = tx.clone();
-        move |message: SharedString| {
-            let value = tx_clone.clone();
-            tokio::spawn(async move {
-                value.send(message.to_string()).expect("receiver quit");
-            });
-        }
-    });
 
-    // Receiving messages from the Tokio task and updating the UI
-    tokio::spawn(async move {
-        let mut message_vector:Vec<String> = vec![];
-        while let Some(received_message) = rx.recv().await 
-        {
-            if let Some(ui) = ui_handle.upgrade() {
 
-                message_vector.push(received_message);
-                let message_vector_clone = message_vector.clone();
-                let the_model : Rc<VecModel<SharedString>> =
-                        Rc::new(VecModel::from(message_vector_clone.into_iter().map(|messages| messages.into()).collect::<Vec<SharedString>>()));
+    //entering tcp stream
+    if let Ok(mut stream) = TcpStream::connect(ADDR).await{
+            println!("Connected to TCP stream: {}{}", stream.local_addr().unwrap().ip(),stream.local_addr().unwrap().ip());
+            let gui = ui_handle.unwrap();
+
+
+               let mut buffer:[u8;1024] = [0;1024];
+               let _ = stream.read(&mut buffer).await.unwrap(); 
+               let message:String = String::from_utf8_lossy(&buffer).to_string();
+               let mut message_vector: Vec<String> = vec![];
+                message_vector.push(message);
+                let the_model : Rc<VecModel<SharedString>> =Rc::new(VecModel::from(message_vector.into_iter().map(|messages| messages.into()).collect::<Vec<SharedString>>()));
                 let the_model_rc = ModelRc::from(the_model.clone());
-            
 
+                gui.set_messages(the_model_rc);
+            //if yk how to read its called on_send_message for a reason
+            gui.on_send_message({
 
-                    ui.set_messages(the_model_rc);
-            }
+                 move |message: SharedString| {
+                let msg_clone = message.clone();
+                let _ = stream.write(msg_clone.to_string().as_bytes());
+                let _ = stream.flush();
+                println!("sent message: {}", msg_clone);
         }
     });
 
-
-
+    ui.run()?;
+    }
+    else{
+        println!("Unsuccessful connection to TCP stream: {}",ADDR);
+    }
 
     // Run the UI loop on the main thread
-    ui.run()?;
 
 
 
@@ -59,8 +57,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 
 
-}
-async fn handle_client(stream: TcpStream){
-    
-    let websocket = accept_async(stream).await ;
-}
+    }
